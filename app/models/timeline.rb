4018092c6f0a0bb47ce4e4fbfe5db6399ab9fa44
robdel12 <robertdeluca19@gmail.com>
2017-12-10 11:24:42 -0600

    Remove instagram from API response

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,7 +2,7 @@ class Timeline
   attr_reader :title, :url, :published_date, :author, :image_url, :description
 
   def self.all
-    timeline = Podcast.all + Post.all + Instagram.all
+    timeline = Podcast.all + Post.all # + Instagram.all
     timeline.sort_by(&:published_date).reverse
   end
 

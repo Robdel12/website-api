d9a9a614e6d8164e13499d5e64f1784f4da47473
Robert DeLuca <robertdeluca19@gmail.com>
2019-05-09 20:25:10 -0500

    Remove podcasts from the timeline response
    Looks like FS broke the API I was relying on to get the data

--- app/models/timeline.rb
+++ app/models/timeline.rb
@@ -2,7 +2,7 @@ class Timeline
   attr_reader :title, :url, :published_date, :author, :image_url, :description
 
   def self.all
-    timeline = Podcast.all + Post.all # + Instagram.all
+    timeline = Post.all # + Instagram.all + Podcast.all
     timeline.sort_by(&:published_date).reverse
   end
 
